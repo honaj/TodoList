f96655d8e7b3f91669d4afe548577625141c26fb
Johan Hellgren <honajnahoj@gmail.com>
2023-08-22 14:48:11 +0200

    Added task sorting

--- Program.cs
+++ Program.cs
@@ -1,49 +1,79 @@
 ﻿using Newtonsoft.Json;
 
+// Path of the JSON file that contains tasks
 const string path = "data/data.json";
 var tasks = await LoadTasks();
 
-MainMenu();
+await MainMenu();
+
 return;
 
-async void MainMenu()
+// This method shows the main menu of the application
+async Task MainMenu()
 {
-    Console.Clear();
-    Console.WriteLine("Welcome to ToDoLy!");
-    Console.WriteLine();
-    Console.WriteLine("Pick an option:");
-    Console.WriteLine("(1) Show Task List");
-    Console.WriteLine("(2) Add New Task");
-    Console.WriteLine("(3) Edit Task (update, mark as completed, remove");
-    Console.WriteLine("(4) Save and Quit");
+    var options = new List<string> { "Show Task List", "Add New Task", "Edit Task (update, mark as completed, remove)", "Save and Quit" };
+    int index = 0;
+
     while (true)
     {
-        switch (Console.ReadKey(true).KeyChar)
+        Console.Clear();
+        Console.WriteLine("Welcome to ToDoLy!");
+        Console.WriteLine();
+        Console.WriteLine("Pick an option:");
+
+        for (int i = 0; i < options.Count; i++)
         {
-            case '1':
-                ShowTasks();
+            if (i == index)
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine(options[i]);
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine(options[i]);
+            }
+        }
+
+        switch (Console.ReadKey(true).Key)
+        {
+            case ConsoleKey.UpArrow:
+                index = (index > 0) ? --index : options.Count - 1;
                 break;
-            case '2':
-                AddTask();
+            case ConsoleKey.DownArrow:
+                index = (index < options.Count - 1) ? ++index : 0;
                 break;
-            case '3':
-                // Edit task logic
+            case ConsoleKey.Enter:
+                switch (index)
+                {
+                    case 0:
+                        await ShowTasks(SortingMode.Unordered);
+                        break;
+                    case 1:
+                        await AddTask();
+                        break;
+                    case 2:
+                        EditTask();
+                        break;
+                    case 3:
+                        await SaveTasks();
+                        Quit();
+                        break;
+                }
                 break;
-            case '4':
-                await SaveTasks();
-                Quit();
-                break;
-        }   
+        }
     }
 }
 
+// Save the serialized tasks to a data.json file
 async Task SaveTasks()
 {
     var tasksJson = JsonConvert.SerializeObject(tasks, Formatting.Indented);
-    // Save the serialized tasks to a data.json file
     await File.WriteAllTextAsync(path, tasksJson);
 }
 
+// This method loads tasks from a json file 
 async Task<List<TodoTask>> LoadTasks()
 {
     if (!File.Exists(path))
@@ -52,44 +82,91 @@ async Task<List<TodoTask>> LoadTasks()
         return new List<TodoTask>();
     }
     var jsonResult = await File.ReadAllTextAsync(path);
-    return JsonConvert.DeserializeObject<List<TodoTask>>(jsonResult);
+    return JsonConvert.DeserializeObject<List<TodoTask>>(jsonResult) ?? throw new InvalidOperationException();
 }
 
-async void ShowTasks()
+// This method displays tasks with specified sorting mode
+async Task ShowTasks(SortingMode mode)
 {
+    Console.Clear();
     tasks = await LoadTasks();
     if (tasks is {Count: 0})
     {
         Console.WriteLine("No tasks have been added");
         return;
     }
-    
-    Console.WriteLine("{0,-10} {1,-10} {2, -20} {3, -10}", "Name", "Project", "Due Date", "Status");
-    foreach (var task in tasks)
+
+    IEnumerable<TodoTask> tasksToView = mode switch
     {
-        Console.WriteLine("{0,-10} {1,-10} {2, -20} {3, -10}", task.Name, task.Project, task.DueDate.ToString(),
-            task.Status);
+        SortingMode.Unordered => tasks,
+        SortingMode.Project => tasks.OrderBy(task => task.Project),
+        SortingMode.Date => tasks.OrderBy(task => task.DueDate),
+        SortingMode.Status => tasks.OrderByDescending(task => task.Status),
+        _ => throw new ArgumentOutOfRangeException(nameof(mode), mode, null)
+    };
+
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine("{0,-20} {1,-20} {2, -20} {3, -20}", "NAME", "PROJECT", "DUE DATE", "STATUS");
+    foreach (var task in tasksToView)
+    {
+        Console.ForegroundColor = task.Status ? ConsoleColor.DarkCyan : ConsoleColor.Magenta;
+        var status = task.Status ? "Completed" : "Pending";
+        Console.WriteLine("{0,-20} {1,-20} {2, -20} {3, -20}", task.Name, task.Project, task.DueDate.ToString(),
+            status);
     }
-    
-    Console.WriteLine("(1) Sort by date");
-    Console.WriteLine("(2) Sort by project");
-    Console.WriteLine("(3) Back to menu");
+
+    var options = new List<string> { "Sort by date", "Sort by project", "Sort by status", "Back to menu" };
+    int index = 0;
+
     while (true)
     {
-        switch (Console.ReadKey(true).KeyChar)
+        Console.ForegroundColor = ConsoleColor.DarkCyan;
+        for (int i = 0; i < options.Count; i++)
         {
-            case '1':
+            if (i == index)
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine(options[i]);
+                Console.ResetColor();
+            }
+            else
+            {
+                Console.WriteLine(options[i]);
+            }
+        }
+
+        switch (Console.ReadKey(true).Key)
+        {
+            case ConsoleKey.UpArrow:
+                index = (index > 0) ? --index : options.Count - 1;
                 break;
-            case '2':
+            case ConsoleKey.DownArrow:
+                index = (index < options.Count - 1) ? ++index : 0;
                 break;
-            case '3':
-                MainMenu();
+            case ConsoleKey.Enter:
+                switch (index)
+                {
+                    case 0:
+                        await ShowTasks(SortingMode.Date);
+                        break;
+                    case 1:
+                        await ShowTasks(SortingMode.Project);
+                        break;
+                    case 2:
+                        await ShowTasks(SortingMode.Status);
+                        break;
+                    case 3:
+                        await MainMenu();
+                        break;
+                }
                 break;
         }
     }
 }
 
-async void AddTask()
+// This method adds a new task to the list
+async Task AddTask()
 {
     string? name;
     while (true)
@@ -127,14 +204,20 @@ async void AddTask()
     var statusInput = Console.ReadLine()?.ToLower();
     var status = statusInput == "yes";
 
-    var task = new TodoTask(name, project, dueDate, status);
+    var task = new TodoTask(name, project, DateOnly.FromDateTime(DateTime.Now), dueDate, status);
     tasks.Add(task);
     await SaveTasks();
 
     Console.WriteLine("Task added successfully!");
-    MainMenu();
+    await MainMenu();
 }
 
+void EditTask()
+{
+    
+}
+
+// This method terminates the application
 void Quit()
 {
     Environment.Exit(0);
@@ -142,16 +225,29 @@ void Quit()
 
 public class TodoTask
 {
+    // properties of the task
     public string? Name { get; set; }
     public string? Project { get; set; }
     public DateOnly DueDate { get; set; }
+    public DateOnly CreationDate { get; set; }
     public bool Status { get; set; }
 
-    public TodoTask(string? name, string? project, DateOnly dueDate, bool status)
+    // constructor of the task
+    public TodoTask(string? name, string? project, DateOnly creationDate, DateOnly dueDate, bool status)
     {
         Name = name;
         Project = project;
+        CreationDate = creationDate;
         DueDate = dueDate;
         Status = status;
     }
+}
+
+// sorting mode enumerator
+internal enum SortingMode
+{
+    Unordered,
+    Project,
+    Date,
+    Status
 }
